4777fd4f44f916ea53dab25d320781e90c4b8769
deejayy <deejayy@deejayy.hu>
2023-07-05 00:26:22 +0200

    Smarter deck generation from sets

--- src/app/feature/blozz/module/deck/helper/deck.helper.ts
+++ src/app/feature/blozz/module/deck/helper/deck.helper.ts
@@ -1,7 +1,9 @@
 import { convertPiece } from '@feature/blozz/module/board/helper/board.helper';
 import { BOX_SIZE, Coord, Piece, TOUCH_DISTANCE } from '@feature/blozz/module/board/model/board.model';
-import { PieceSetVariant, RotateDirection, pieceSetVariants, pieceSets, rotateDirection } from '@feature/blozz/module/deck/model/piece-set';
-import { GameMode } from '@feature/blozz/module/settings/model/settings.model';
+import { RotateDirection, pieceSets, rotateDirection } from '@feature/blozz/module/deck/model/piece-set';
+import { GameMode, gameModes } from '@feature/blozz/module/settings/model/settings.model';
+
+const ROTATION_AMOUNT = 3;
 
 export const getTargetTouch = (event: TouchEvent): { targetX: number; targetY: number } => {
   const touch = event.targetTouches[0] ?? event.changedTouches[0];
@@ -30,21 +32,6 @@ export const getTouchCoords = (event: TouchEvent, relativeTo: Coord = { x: 0, y:
   };
 };
 
-export const pieceWidth = (value: number[][]) => value.reduce((acc, curr) => (curr.length > acc.length ? curr : acc), []);
-
-export const generatePiece = (pieceSet: PieceSetVariant = pieceSetVariants.STANDARD): Piece => {
-  const selectedPieceSet = pieceSets[pieceSet];
-  if (pieceSet === pieceSetVariants.EXTREME) {
-    return Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => Math.round(Math.random())));
-  }
-
-  return convertPiece(selectedPieceSet[Math.floor(Math.random() * selectedPieceSet.length)]!);
-};
-
-export const generatePieces = (amount: number, gameMode: GameMode) => {
-  return Array.from({ length: amount }, () => generatePiece(gameMode));
-};
-
 export const rotatePiece = (piece: Piece, direction: RotateDirection = rotateDirection.RIGHT): Piece => {
   const transpose = (m: Piece): Piece => m[0]!.map((_, i) => m.map((x) => x[i]!).reverse());
 
@@ -54,4 +41,28 @@ export const rotatePiece = (piece: Piece, direction: RotateDirection = rotateDir
 
   console.error('Not implemented');
   return piece;
+};
+
+export const pieceWidth = (value: number[][]) => value.reduce((acc, curr) => (curr.length > acc.length ? curr : acc), []);
+
+export const randomizedPiece = () => {
+  return Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => Math.round(Math.random())));
+};
+
+export const generatePieces = (amount: number, gameMode: GameMode) => {
+  const selectedPieceSet = [...pieceSets[gameMode]];
+  return Array.from({ length: amount }, () => {
+    if (gameMode === gameModes.EXTREME) {
+      return randomizedPiece();
+    }
+
+    const pieceIndex = Math.floor(Math.random() * selectedPieceSet.length);
+    const pickedPiece = selectedPieceSet.splice(pieceIndex, 1)[0] ?? '.';
+
+    return convertPiece(pickedPiece);
+  }).map((piece: Piece) => {
+    let newPiece = piece;
+    Array.from({ length: Math.round(Math.random() * ROTATION_AMOUNT) }, () => (newPiece = rotatePiece(newPiece)));
+    return newPiece;
+  });
 };
